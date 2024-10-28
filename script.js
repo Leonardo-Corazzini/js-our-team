b7685b69e53fc7f46d0f52085b8b89116e27459c
Leonardo-Corazzini <leocora10@gmail.com>
2024-10-28 17:33:30 +0100

    ADD: responsive e generazione card

--- script.js
+++ script.js
@@ -37,20 +37,53 @@ const teamMembers = [
   }
 ];
 const cardRow = document.getElementById('card-row')
+const formCard = document.getElementById('form-card')
 
-for(let i = 0; i < teamMembers.length;i++){
- let element = teamMembers[i]
-  const card =`
-  <div class="col-4 row">
+for (let i = 0; i < teamMembers.length; i++) {
+  let element = teamMembers[i]
+  const card = `
+ <div class="bg-dark text-white col-md-6 col-lg-4 mb-3">
+  <div class="row g-0">
     <div class="col-4">
       <img src="${element.img}" class="img-fluid rounded-start" alt="...">
     </div>
     <div class="col-8">
-      <h5 class="card-title">${element.name}</h5>
-      <p class="card-text">${element.role}</p>
-      <p class="card-text">${element.email}</p>
+      <div class="card-body">
+        <h5 class="card-title">${element.name}</h5>
+        <p class="card-text">${element.role}</p>
+        <p class="card-text">${element.email}</p>
+      </div>
     </div>
-
-  </div>`
+  </div>
+</div>`
   cardRow.innerHTML += card
+}
+
+formCard.addEventListener('submit' , function (event){
+  event.preventDefault()
+  const cardName = document.getElementById('card-name')
+  const cardRole = document.getElementById('card-role')
+  const cardEmail = document.getElementById('card-email')
+  const formFile = document.getElementById('form-File')
+  cardGenerator(cardName.value,cardRole.value,cardEmail.value,formFile)
+})
+
+
+function cardGenerator (name,rule,email,img){
+  const newCard = `
+  <div class="bg-dark text-white col-md-6 col-lg-4 mb-3">
+   <div class="row g-0">
+     <div class="col-4">
+       <img src="${img}" class="img-fluid rounded-start" alt="...">
+     </div>
+     <div class="col-8">
+       <div class="card-body">
+         <h5 class="card-title">${name}</h5>
+         <p class="card-text">${rule}</p>
+         <p class="card-text">${email}</p>
+       </div>
+     </div>
+   </div>
+ </div>`
+   cardRow.innerHTML += newCard
 }
